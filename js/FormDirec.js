faf6ca83fa7f6cd0635eacbd40a52f5ebc5794bd
LuiGio <24hdz.gio@gmail.com>
2023-07-26 12:12:30 -0600

    Se creo la pagina de direccion de usuario

---
+++ js/FormDirec.js
@@ -0,0 +1,58 @@
+// Función para validar el formulario
+function validateForm(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    // Obtener los valores de los campos del formulario
+    const calleNumero = document.getElementById("calle_numero").value;
+    const codigoPostal = document.getElementById("codigo_postal").value;
+    const ciudad = document.getElementById("ciudad").value;
+    const estado = document.getElementById("estado").value;
+
+    // Validar que los campos no estén vacíos
+    if (!calleNumero) {
+        showAlert("Por favor, ingresa la calle y el número.");
+        return;
+    }
+
+    if (!codigoPostal) {
+        showAlert("Por favor, ingresa el código postal.");
+        return;
+    }
+
+    // Validar el código postal con una expresión regular
+    const codigoPostalPattern = /^\d{5}$/;
+    if (!codigoPostalPattern.test(codigoPostal)) {
+        showAlert("Por favor, ingresa un código postal válido de 5 dígitos.");
+        return;
+    }
+
+    if (!ciudad) {
+        showAlert("Por favor, ingresa la ciudad.");
+        return;
+    }
+
+    if (estado === "") {
+        showAlert("Por favor, selecciona un estado.");
+        return;
+    }
+
+    // Si todos los campos están completos, puedes enviar el formulario aquí
+    // Ejemplo: event.target.submit();
+
+    showAlert("Formulario guardado correctamente.", "success");
+}
+
+// Función para mostrar mensajes de alerta
+function showAlert(message, type = "danger") {
+    const alertContainer = document.getElementById("alertContainer");
+    alertContainer.innerHTML = `<div class="alert alert-${type}" role="alert">${message}</div>`;
+
+    // Limpiar el mensaje de alerta después de 3 segundos
+    setTimeout(() => {
+        alertContainer.innerHTML = "";
+    }, 3000);
+}
+
+// Agregar el evento de validación al formulario cuando se envíe
+const form = document.querySelector("form");
+form.addEventListener("submit", validateForm);
